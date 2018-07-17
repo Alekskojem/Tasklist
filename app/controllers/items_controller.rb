0995fa37a749e7d42cf655aba62f9b7b61236a26
Aleksandr Kojemyako <alekskojem@gmail.com>
2018-07-18 00:14:13 +0300

    Add functionality fro complete items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,11 @@ end
     redirect_to root_path
   end
   
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end
 
   private
 
